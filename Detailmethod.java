1e4eba73a033341bbd24386ba96b66ef5a27bece
DeepikaAiluri <deepika4sri@gmail.com>
2019-05-07 17:05:04 -0500

    Initial commit

---
+++ Detailmethod.java
@@ -0,0 +1,17 @@
+package eSP;
+
+public class Detailmethod {
+	
+	
+	int sal;
+	int bon;
+	String name;
+	
+	
+	public void total() {
+		System.out.println("total salary for "+  name+ " is"   +(sal+bon));
+		
+		
+	}
+
+}
---
+++ Emp.java
@@ -0,0 +1,25 @@
+package eSP;
+
+public class Emp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Detailmethod E1=new Detailmethod();
+		
+		Detailmethod E2=new Detailmethod();
+		
+		E1.sal=100;
+		E1.bon=5;
+		E1.name="Deepika";
+		E1.total();
+		
+		E2.sal=200;
+		E2.bon=50;
+		E2.name="Sam";
+		E2.total();
+
+	}
+
+}
